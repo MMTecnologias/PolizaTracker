9d5d95720701a71d0212fec41ecb82710f78f3b0
AbigailDC <70286570+AbigailDC@users.noreply.github.com>
2024-02-11 17:29:14 -0600

    Actualización Funciones Login

--- flaskapp/app/static/assets/controladores/login2.js
+++ flaskapp/app/static/assets/controladores/login2.js
@@ -11,3 +11,21 @@ function validarFormulario() {
         document.getElementById('').style.display = 'block';
     }
 }
+
+
+function enviarValidacion(){
+    var usuarioIngresado = document.getElementById('InputUsuario').value;
+    if (usuarioIngresado.trim() === "Abi") {
+        Swal.fire({
+            title: '¡Solicitud Enviada!',
+            text: 'Revisa tu correo, el administrador te hará llegar una nueva contraseña',
+            icon: 'success',
+        });
+    } else {
+        Swal.fire({
+            title: '¡Error!',
+            text: 'Hubo un problema, y no se pudo enviar tu solicitud',
+            icon: 'error',
+        });
+    }
+}
