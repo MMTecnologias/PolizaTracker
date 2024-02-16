cbe76ef0b2a39fc07c6aa98fa2693742efed95b3
AbigailDC <70286570+AbigailDC@users.noreply.github.com>
2024-02-16 13:04:15 -0600

    Actualizacion Script

--- flaskapp/app/static/assets/js/scripts.js
+++ flaskapp/app/static/assets/js/scripts.js
@@ -1,18 +1,5 @@
 
-
-onload=function(){
-    var e=document.getElementById("refreshed");
-    if(e.value=="no")e.value="yes";
-    else{e.value="no";location.reload();}
-}
-
-window.addEventListener('pageshow', function(event) {
-    if (event.persisted) {
-        window.location.reload();
-    }
-});
-
-(function($) {
+(function ($) {
     "use strict";
 
     /*================================
@@ -20,9 +7,9 @@ window.addEventListener('pageshow', function(event) {
     ==================================*/
 
     var preloader = $('#preloader');
-    $(window).on('load', function() {
-        setTimeout(function() {
-            preloader.fadeOut('slow', function() { $(this).remove(); });
+    $(window).on('load', function () {
+        setTimeout(function () {
+            preloader.fadeOut('slow', function () { $(this).remove(); });
         }, 300)
     });
 
@@ -32,14 +19,14 @@ window.addEventListener('pageshow', function(event) {
     if (window.innerWidth <= 1364) {
         $('.page-container').addClass('sbar_collapsed');
     }
-    $('.nav-btn').on('click', function() {
+    $('.nav-btn').on('click', function () {
         $('.page-container').toggleClass('sbar_collapsed');
     });
 
     /*================================
     Start Footer resizer
     ==================================*/
-    var e = function() {
+    var e = function () {
         var e = (window.innerHeight > 0 ? window.innerHeight : this.screen.height) - 5;
         (e -= 67) < 1 && (e = 1), e > 67 && $(".main-content").css("min-height", e + "px")
     };
@@ -72,7 +59,7 @@ window.addEventListener('pageshow', function(event) {
     /*================================
     stickey Header
     ==================================*/
-    $(window).on('scroll', function() {
+    $(window).on('scroll', function () {
         var scroll = $(window).scrollTop(),
             mainHeader = $('#sticky-header'),
             mainHeaderHeight = mainHeader.innerHeight();
@@ -91,12 +78,12 @@ window.addEventListener('pageshow', function(event) {
     $('[data-toggle="popover"]').popover()
 
     /*------------- Start form Validation -------------*/
-    window.addEventListener('load', function() {
+    window.addEventListener('load', function () {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
         var forms = document.getElementsByClassName('needs-validation');
         // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function(form) {
-            form.addEventListener('submit', function(event) {
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
                 if (form.checkValidity() === false) {
                     event.preventDefault();
                     event.stopPropagation();
@@ -136,10 +123,10 @@ window.addEventListener('pageshow', function(event) {
     /*================================
     login form
     ==================================*/
-    $('.form-gp input').on('focus', function() {
+    $('.form-gp input').on('focus', function () {
         $(this).parent('.form-gp').addClass('focused');
     });
-    $('.form-gp input').on('focusout', function() {
+    $('.form-gp input').on('focusout', function () {
         if ($(this).val().length === 0) {
             $(this).parent('.form-gp').removeClass('focused');
         }
@@ -148,7 +135,7 @@ window.addEventListener('pageshow', function(event) {
     /*================================
     slider-area background setting
     ==================================*/
-    $('.settings-btn, .offset-close').on('click', function() {
+    $('.settings-btn, .offset-close').on('click', function () {
         $('.offset-area').toggleClass('show_hide');
         $('.settings-btn').toggleClass('active');
     });
@@ -193,7 +180,7 @@ window.addEventListener('pageshow', function(event) {
 
     if ($('#full-view').length) {
 
-        var requestFullscreen = function(ele) {
+        var requestFullscreen = function (ele) {
             if (ele.requestFullscreen) {
                 ele.requestFullscreen();
             } else if (ele.webkitRequestFullscreen) {
@@ -207,7 +194,7 @@ window.addEventListener('pageshow', function(event) {
             }
         };
 
-        var exitFullscreen = function() {
+        var exitFullscreen = function () {
             if (document.exitFullscreen) {
                 document.exitFullscreen();
             } else if (document.webkitExitFullscreen) {
@@ -224,17 +211,76 @@ window.addEventListener('pageshow', function(event) {
         var fsDocButton = document.getElementById('full-view');
         var fsExitDocButton = document.getElementById('full-view-exit');
 
-        fsDocButton.addEventListener('click', function(e) {
+        fsDocButton.addEventListener('click', function (e) {
             e.preventDefault();
             requestFullscreen(document.documentElement);
             $('body').addClass('expanded');
         });
 
-        fsExitDocButton.addEventListener('click', function(e) {
+        fsExitDocButton.addEventListener('click', function (e) {
             e.preventDefault();
             exitFullscreen();
             $('body').removeClass('expanded');
         });
     }
+
+    $("#login").modal({
+        modal: true,
+        buttons: {
+            Ok: function () {
+                $(this).modal("close");
+            }
+        }
+    });
+
+    (function () {
+        $(function () {
+
+            $('botonModal1').on('click', function () {
+                $('exampleModal').modal();
+            });
+        });
+    }());
+
+    const usersDatabase = {
+        "Abi": {
+            contraseña: "1234",
+            rol: "admin"
+        },
+        "Luis": {
+            contraseña: "qwerty",
+            rol: "gerente"
+        },
+    };
+
+    document.addEventListener('DOMContentLoaded', function () {
+        // Agregar evento de escucha al formulario
+        document.getElementById('loginform').addEventListener('submit', function (event) {
+            event.preventDefault(); // Evitar que el formulario se envíe por defecto
+
+            // Obtener valores del usuario y la contraseña
+            var usuario = document.getElementById('usuarioAutent').value;
+            var password = document.getElementById('passwordAutent').value;
+
+            // Verificar credenciales
+            if (usersDatabase.hasOwnProperty(usuario) && usersDatabase[usuario].contraseña===password) {
+                // Otras acciones después del inicio de sesión (puedes redirigir a otra página)
+                sessionStorage.setItem('nivelUsuario',usersDatabase[usuario].rol);
+                window.location.href = 'menuP.html';
+            } else {
+                // Credenciales no válidas, mostrar mensaje de error
+                document.getElementById('mensajeError').style.display = 'block';
+                document.getElementById('mensajeError').innerText = 'Usuario o contraseña incorrectos';
+            }
+        });
+    });
+
+    const liAdmin = document.getElementById('liAdmin');
+    if (nivelUsuario === 'Administrador') {
+        liAdmin.style.display = 'block';
+    } else {
+        liAdmin.style.display = 'none';
+    }
+
 
 })(jQuery);
