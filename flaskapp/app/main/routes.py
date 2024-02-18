9281e05ab57960737bfbb35e12dc2fffeac90ee6
Luis <you@example.com>
2024-02-18 10:15:19 -0600

    menu

--- flaskapp/app/main/routes.py
+++ flaskapp/app/main/routes.py
@@ -38,6 +38,12 @@ class ClientRegistrationForm(FlaskForm):
     giro_actividad = StringField('giro_actividad', validators=[Length(max=30)])
     nuevo_grupo = StringField('nuevo_grupo', validators=[Length(max=30)])  # Add this field for new group input
 
+@main.route('/usuario', methods=['GET', 'POST'])
+@login_required
+def usuario():
+    return render_template('usuario.html', user=current_user)
+
+
 
     
 @main.route('/cliente', methods=['GET', 'POST'])
