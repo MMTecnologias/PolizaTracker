8096e0f7448ca8c760ff9740519d89780442f0d6
isbaruc <isbaruc@proton.me>
2024-02-25 16:57:38 -0600

    🎨 Agrega leyenda de contrasenia invalida

--- flaskapp/app/static/assets/controladores/login2.js
+++ flaskapp/app/static/assets/controladores/login2.js
@@ -1,7 +1,5 @@
-
-
-$(document).ready(function() {
-    $("#loginform").submit(function(e) {
+$(document).ready(function () {
+   $('#loginform').submit(function (e) {
       //---------------^---------------
       e.preventDefault();
 
@@ -10,37 +8,39 @@ $(document).ready(function() {
 
       // Send AJAX POST request
       $.ajax({
-          type: 'POST',
-          url: 'login_ajax',  // Flask route for login
-          data: formData,
-          success: function(response) {
-              // Check response for success or error
-              if (response.success) {
-                  // Redirect user to the desired page
-                  window.location.href = response.redirect;
-              } else {
-                  // Display error message
-                  $('#mensajeError').css('display', 'block');
-              }
-          },
-          error: function(xhr, status, error) {
-              Swal.fire({
-                  title: 'Error inesperado',
-                  text: 'Lamentamos el inconveniente, porfavor vuelve a intentarlo',
-                  icon: 'error',
-              });
-          }
+         type: 'POST',
+         url: 'login_ajax', // Flask route for login
+         data: formData,
+         success: function (response) {
+            // Check response for success or error
+            if (response.success) {
+               // Redirect user to the desired page
+               window.location.href = response.redirect;
+            } else {
+               // Display error message
+               $('#mensajeError').css('opacity', '1');
+               //espera 3 segundos y oculta
+               setTimeout(function () {
+                  $('#mensajeError').css('opacity', '0');
+               }, 3000);
+            }
+         },
+         error: function (xhr, status, error) {
+            Swal.fire({
+               title: 'Error inesperado',
+               text: 'Lamentamos el inconveniente, porfavor vuelve a intentarlo',
+               icon: 'error'
+            });
+         }
       });
-      
+
       // Return false to prevent form submission
       return false;
-  
-    });
-  });
+   });
+});
 
-
-$(document).ready(function() {
-    $("#forgotpassform").submit(function(e) {
+$(document).ready(function () {
+   $('#forgotpassform').submit(function (e) {
       //---------------^---------------
       e.preventDefault();
 
@@ -48,52 +48,44 @@ $(document).ready(function() {
       var formData = $('#forgotpassform').serialize();
 
       $.ajax({
-              type: 'POST',
-              url: 'forgotpass_ajax',  // Flask route for login
-              data: formData,
-              success: function(response) {
-                  // Check response for success or error
-                  if (response.correctuser) {
-                      if(response.new){
-                          Swal.fire({
-                              title: '¡Solicitud Enviada!',
-                              text: 'Revisa tu correo, el administrador te hará llegar una nueva contraseña',
-                              icon: 'success',
-                          }).then(function() {
-                              window.location.href = response.redirect;
-                          });
-                      } else{
-                          Swal.fire({
-                              title: 'Ya tiene una solicitud Pendiente',
-                              text: 'Revisa tu correo, el administrador te hará llegar una nueva contraseña',
-                              icon: 'success',
-                          }).then(function() {
-                              window.location.href = response.redirect;
-                          });
-                      }
-                  } else {
-                      // Display error message
-                      $('#mensajeError').css('display', 'block');
-                  }
-              },
-              error: function(xhr, status, error) {
+         type: 'POST',
+         url: 'forgotpass_ajax', // Flask route for login
+         data: formData,
+         success: function (response) {
+            // Check response for success or error
+            if (response.correctuser) {
+               if (response.new) {
                   Swal.fire({
-                      title: 'Error inesperado',
-                      text: 'Lamentamos el inconveniente, porfavor vuelve a intentarlo',
-                      icon: 'error',
+                     title: '¡Solicitud Enviada!',
+                     text: 'Revisa tu correo, el administrador te hará llegar una nueva contraseña',
+                     icon: 'success'
+                  }).then(function () {
+                     window.location.href = response.redirect;
                   });
-              }
-          });
-      
+               } else {
+                  Swal.fire({
+                     title: 'Ya tiene una solicitud Pendiente',
+                     text: 'Revisa tu correo, el administrador te hará llegar una nueva contraseña',
+                     icon: 'success'
+                  }).then(function () {
+                     window.location.href = response.redirect;
+                  });
+               }
+            } else {
+               // Display error message
+               $('#mensajeError').css('display', 'block');
+            }
+         },
+         error: function (xhr, status, error) {
+            Swal.fire({
+               title: 'Error inesperado',
+               text: 'Lamentamos el inconveniente, porfavor vuelve a intentarlo',
+               icon: 'error'
+            });
+         }
+      });
+
       // Return false to prevent form submission
       return false;
-  
-    });
-  });
-
-
-
-
-
-  
-
+   });
+});
