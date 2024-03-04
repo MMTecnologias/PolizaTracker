dc74ee6bb018d82d9791e4a70c088ef50f0524e2
isbaruc <isbaruc@proton.me>
2024-03-04 15:21:04 -0600

    🔧 Ajustes para correr el proyecto

--- flaskapp/app/config.py
+++ flaskapp/app/config.py
@@ -1,4 +1,4 @@
 SECRET_KEY = 'TKNPSSWRDKYGGSYSTM4599OEYE'
 #//username:password@server/db
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://brc:Pebble5$025896321@localhost:3306/polizatracker'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:QuiJote0627!@localhost:3306/polizatracker'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
