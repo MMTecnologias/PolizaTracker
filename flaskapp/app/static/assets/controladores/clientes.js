054cc4cc16d7f2dabd30308aec5bd7740a705df6
isbaruc <isbaruc@proton.me>
2024-04-14 00:27:56 -0600

    🎨 implementa asincronismo

--- flaskapp/app/static/assets/controladores/clientes.js
+++ flaskapp/app/static/assets/controladores/clientes.js
@@ -8,10 +8,11 @@ function currentIndexToShow(currentIndex) {
    return currentIndex + 1;
 }
 
-function pintarTabla(index) {
-   //Reiniciamos la tabla al ejecutar la función
-   document.querySelector('#demo').innerHTML = '';
-   fetch('/get_clients_data2', {
+const currentPageData = async () => {
+   let clientData = [];
+   const index = currentIndex;
+   //Solicitamos los datos
+   const currentPageData = await fetch('/get_clients_data2', {
       method: 'POST',
       headers: {
          'Content-Type': 'application/x-www-form-urlencoded'
@@ -20,73 +21,142 @@ function pintarTabla(index) {
          start: `${index === undefined ? 0 : index * clientsPerPage}`,
          length: clientsPerPage
       })
-   })
-      .then((response) => response.json())
-      .then(function (data) {
-         for (var i = 0; i < data[1].length; i++) {
-            console.log(data[1][i].id + ' ' + data[1][i].fullname);
-            document.querySelector('#demo').innerHTML += `
-            
-               <tr  class="tableOption">
-                 
-                     
-                     <td>${data[1][i]['fullname']}</td>
-                     <td>${data[1][i]['correo']}</td>
-                     <td>${data[1][i]['tel_movil']}</td>
-                     <td>
-                     <!-- Este solo se debe mostrar al genrente -->
-                        <ul class="btn_table_options">
-                           <li>
-                              <a href="#" class="btn__icon_delete" id="btnDelete_${data[1][i].id}">
-                                 <svg class="noClickable" xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 -960 960 960" width="21" class="btn_icon"><path d="M292.309-140.001q-29.923 0-51.115-21.193-21.193-21.192-21.193-51.115V-720h-40v-59.999H360v-35.384h240v35.384h179.999V-720h-40v507.691q0 30.308-21 51.308t-51.308 21H292.309ZM680-720H280v507.691q0 5.385 3.462 8.847 3.462 3.462 8.847 3.462h375.382q4.616 0 8.463-3.846 3.846-3.847 3.846-8.463V-720ZM376.155-280h59.999v-360h-59.999v360Zm147.691 0h59.999v-360h-59.999v360ZM280-720v520-520Z"/></svg>
-                              </a>
-                           </li>
-                           <li>
-                              <a href="#" class="btn__icon_edit" id="btnEdit_${data[1][i].id}">
-                                 <svg class="noClickable" xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 -960 960 960" width="21" class="btn_icon"><path d="M200-200h50.461l409.463-409.463-50.461-50.461L200-250.461V-200Zm-59.999 59.999v-135.383l527.616-527.384q9.073-8.241 20.036-12.736 10.963-4.495 22.993-4.495 12.029 0 23.307 4.27 11.277 4.269 19.969 13.576l48.846 49.461q9.308 8.692 13.269 20.004 3.962 11.311 3.962 22.622 0 12.065-4.121 23.028-4.12 10.964-13.11 20.037l-527.384 527H140.001Zm620.384-570.153-50.231-50.231 50.231 50.231Zm-126.134 75.903-24.788-25.673 50.461 50.461-25.673-24.788Z"/></svg> 
-                              </a>
-                           </li>
-                           <li>
-                              <a href="#" class="btn__icon_show">
-                                 <svg class="noClickable" xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 -960 960 960" width="21" class="btn_icon"><path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"/></svg>
-                              </a>
-                           </li>
-                        </ul>
-                     </td>
-                  
-               </tr>
-               
-            `;
-         }
+   });
 
-         //Eliminar cliente desde la tabla
-         const btnDelete = document.querySelectorAll('.btn__icon_delete');
+   //Convertimos los datos a JSON
+   let data = await currentPageData.json();
 
-         btnDelete.forEach((btn) => {
-            btn.addEventListener('click', (event) => {
-               console.log(event.target.id.split('_')[1]);
-               //función eliminar
-               deleteClient(event.target.id.split('_')[1]);
-            });
-         });
+   //Creamos un objeto llamado ClientData y lo llenamos iterando en la data JSON
 
-         //Editar cliente desde la tabla
-         const btnEdit = document.querySelectorAll('.btn__icon_edit');
-
-         btnEdit.forEach((btn) => {
-            btn.addEventListener('click', (event) => {
-               console.log(
-                  `se ha realizado la consulta, id enviado: ${
-                     event.target.id.split('_')[1]
-                  }`
-               );
-               editClient(event.target.id.split('_')[1]);
-            });
-         });
+   //Rellenamos el arreglo "clientData" con los datos del servidor
+   data[1].forEach((client) => {
+      clientData.push({
+         'fullname': client.fullname,
+         'mail': client.correo,
+         'phone': client.tel_movil
       });
+   });
 
-   verGrupos();
-}
+   return clientData;
+};
+
+const fillTable = async () => {
+   const currentData = await currentPageData();
+
+   currentData.forEach((client) => {
+      document.querySelector('#demo').innerHTML += `
+               
+                  <tr  class="tableOption">
+                    
+                        
+                        <td>${client.fullname}</td>
+                        <td>${client.mail}</td>
+                        <td>${client.phone}</td>
+                        <td>
+                        <!-- Este solo se debe mostrar al genrente -->
+                           <ul class="btn_table_options">
+                              <li>
+                                 <a href="#" class="btn__icon_delete" id="btnDelete_${client.id}">
+                                    <svg class="noClickable" xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 -960 960 960" width="21" class="btn_icon"><path d="M292.309-140.001q-29.923 0-51.115-21.193-21.193-21.192-21.193-51.115V-720h-40v-59.999H360v-35.384h240v35.384h179.999V-720h-40v507.691q0 30.308-21 51.308t-51.308 21H292.309ZM680-720H280v507.691q0 5.385 3.462 8.847 3.462 3.462 8.847 3.462h375.382q4.616 0 8.463-3.846 3.846-3.847 3.846-8.463V-720ZM376.155-280h59.999v-360h-59.999v360Zm147.691 0h59.999v-360h-59.999v360ZM280-720v520-520Z"/></svg>
+                                 </a>
+                              </li>
+                              <li>
+                                 <a href="#" class="btn__icon_edit" id="btnEdit_${client.id}">
+                                    <svg class="noClickable" xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 -960 960 960" width="21" class="btn_icon"><path d="M200-200h50.461l409.463-409.463-50.461-50.461L200-250.461V-200Zm-59.999 59.999v-135.383l527.616-527.384q9.073-8.241 20.036-12.736 10.963-4.495 22.993-4.495 12.029 0 23.307 4.27 11.277 4.269 19.969 13.576l48.846 49.461q9.308 8.692 13.269 20.004 3.962 11.311 3.962 22.622 0 12.065-4.121 23.028-4.12 10.964-13.11 20.037l-527.384 527H140.001Zm620.384-570.153-50.231-50.231 50.231 50.231Zm-126.134 75.903-24.788-25.673 50.461 50.461-25.673-24.788Z"/></svg> 
+                                 </a>
+                              </li>
+                              <li>
+                                 <a href="#" class="btn__icon_show">
+                                    <svg class="noClickable" xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 -960 960 960" width="21" class="btn_icon"><path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"/></svg>
+                                 </a>
+                              </li>
+                           </ul>
+                        </td>
+                     
+                  </tr>`;
+   });
+};
+
+fillTable();
+
+// async function pintarTabla(index) {
+//    //Reiniciamos la tabla al ejecutar la función
+//    document.querySelector('#demo').innerHTML = '';
+//    await fetch('/get_clients_data2', {
+//       method: 'POST',
+//       headers: {
+//          'Content-Type': 'application/x-www-form-urlencoded'
+//       },
+//       body: new URLSearchParams({
+//          start: `${index === undefined ? 0 : index * clientsPerPage}`,
+//          length: clientsPerPage
+//       })
+//    })
+//       .then((response) => response.json())
+//       .then(function (data) {
+//          for (var i = 0; i < data[1].length; i++) {
+//             console.log(data[1][i].id + ' ' + data[1][i].fullname);
+//             document.querySelector('#demo').innerHTML += `
+
+//                <tr  class="tableOption">
+
+//                      <td>${data[1][i]['fullname']}</td>
+//                      <td>${data[1][i]['correo']}</td>
+//                      <td>${data[1][i]['tel_movil']}</td>
+//                      <td>
+//                      <!-- Este solo se debe mostrar al genrente -->
+//                         <ul class="btn_table_options">
+//                            <li>
+//                               <a href="#" class="btn__icon_delete" id="btnDelete_${data[1][i].id}">
+//                                  <svg class="noClickable" xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 -960 960 960" width="21" class="btn_icon"><path d="M292.309-140.001q-29.923 0-51.115-21.193-21.193-21.192-21.193-51.115V-720h-40v-59.999H360v-35.384h240v35.384h179.999V-720h-40v507.691q0 30.308-21 51.308t-51.308 21H292.309ZM680-720H280v507.691q0 5.385 3.462 8.847 3.462 3.462 8.847 3.462h375.382q4.616 0 8.463-3.846 3.846-3.847 3.846-8.463V-720ZM376.155-280h59.999v-360h-59.999v360Zm147.691 0h59.999v-360h-59.999v360ZM280-720v520-520Z"/></svg>
+//                               </a>
+//                            </li>
+//                            <li>
+//                               <a href="#" class="btn__icon_edit" id="btnEdit_${data[1][i].id}">
+//                                  <svg class="noClickable" xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 -960 960 960" width="21" class="btn_icon"><path d="M200-200h50.461l409.463-409.463-50.461-50.461L200-250.461V-200Zm-59.999 59.999v-135.383l527.616-527.384q9.073-8.241 20.036-12.736 10.963-4.495 22.993-4.495 12.029 0 23.307 4.27 11.277 4.269 19.969 13.576l48.846 49.461q9.308 8.692 13.269 20.004 3.962 11.311 3.962 22.622 0 12.065-4.121 23.028-4.12 10.964-13.11 20.037l-527.384 527H140.001Zm620.384-570.153-50.231-50.231 50.231 50.231Zm-126.134 75.903-24.788-25.673 50.461 50.461-25.673-24.788Z"/></svg>
+//                               </a>
+//                            </li>
+//                            <li>
+//                               <a href="#" class="btn__icon_show">
+//                                  <svg class="noClickable" xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 -960 960 960" width="21" class="btn_icon"><path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"/></svg>
+//                               </a>
+//                            </li>
+//                         </ul>
+//                      </td>
+
+//                </tr>
+
+//             `;
+//          }
+
+//          //Eliminar cliente desde la tabla
+//          const btnDelete = document.querySelectorAll('.btn__icon_delete');
+
+//          btnDelete.forEach((btn) => {
+//             btn.addEventListener('click', (event) => {
+//                console.log(event.target.id.split('_')[1]);
+//                //función eliminar
+//                deleteClient(event.target.id.split('_')[1]);
+//             });
+//          });
+
+//          //Editar cliente desde la tabla
+//          const btnEdit = document.querySelectorAll('.btn__icon_edit');
+
+//          btnEdit.forEach((btn) => {
+//             btn.addEventListener('click', (event) => {
+//                console.log(
+//                   `se ha realizado la consulta, id enviado: ${
+//                      event.target.id.split('_')[1]
+//                   }`
+//                );
+//                editClient(event.target.id.split('_')[1]);
+//             });
+//          });
+//       });
+
+//    verGrupos();
+// }
 
 pintarTabla();
 
@@ -99,10 +169,10 @@ function verGrupos() {
 }
 
 //Función para eliminar cliente
-function deleteClient(id) {
+async function deleteClient(id) {
    //insertar función eliminar
 
-   fetch('/delete_client', {
+   await fetch('/delete_client', {
       method: 'POST',
       headers: {
          'Content-Type': 'application/x-www-form-urlencoded'
@@ -112,11 +182,12 @@ function deleteClient(id) {
       })
    })
       .then(console.log('cliente eliminado'))
-      .then(
-         setTimeout(() => {
-            pintarTabla(currentIndex);
-         }, 500)
-      );
+      .then(await pintarTabla(currentIndex));
+   // .then(
+   //    setTimeout(() => {
+   //       pintarTabla(currentIndex);
+   //    }, 500)
+   // );
 }
 
 //Función para editar cliente
