1d21729e43b8dd846ad05cb8ca7bd4e129211e1d
isbaruc <isbaruc@proton.me>
2024-05-08 05:49:34 -0600

    🙈 ignora config.py

--- flaskapp/app/config.py
+++
@@ -1,6 +0,0 @@
-SECRET_KEY = 'TKNPSSWRDKYGGSYSTM4599OEYE'
-#//username:password@server/db
-#mysql+mysqlconnector://root:QuiJote0627!@localhost:3306/polizatracker
-#mysql+mysqlconnector://ggsys:12GGsysGG21@localhost:3306/GGsystem
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://ggsys:12GGsysGG21@localhost:3306/GGsystem'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
