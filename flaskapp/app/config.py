d4b12804f83abcd6c553249ddc3f9128317be9d4
isbaruc <isbaruc@proton.me>
2024-05-16 16:07:43 -0600

    🐛 elimina config.py del seguimiento git

--- flaskapp/app/config.py
+++
@@ -1,5 +0,0 @@
-SECRET_KEY = 'TKNPSSWRDKYGGSYSTM4599OEYE'
-#//username:password@server/db
-#mysql+mysqlconnector://root:QuiJote0627!@localhost:3306/polizatracker
-#mysql+mysqlconnector://ggsys:12GGsysGG21@localhost:3306/GGsystem
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:1234@localhost:3306/ggsystem'
