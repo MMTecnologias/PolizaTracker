c9d12c38f8b6b6274f3f6c2ea444e705e4defd62
Abi <abigail.diaz@ulv.edu.mx>
2024-06-07 09:25:57 -0600

    Modal de historial de cambios

--- flaskapp/app/static/assets/controladores/usuario.js
+++ flaskapp/app/static/assets/controladores/usuario.js
@@ -91,7 +91,7 @@ const updateTable = async (userData) => {
                                  </a>
                               </li>
                               <li>
-                                 <a href="#" class="btn__icon_show" id="btnShow_${usuario.id}">
+                                 <a href="#" class="btn__icon_show1" id="btnShow_${usuario.id}">
                                     <svg class="noClickable" xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 -960 960 960" width="21" class="btn_icon"><path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"/></svg>
                                  </a>
                               </li>
@@ -100,7 +100,7 @@ const updateTable = async (userData) => {
    });
    await addBtnDelete();
    await addBtnEdit();
-   //await addBtnShow();
+   await addBtnHistorial();
 
 };
 
@@ -219,18 +219,6 @@ const editClient = async (id) => {
 //     });
    
 };
-
-/* const addBtnShow = async () => {
-   const btnShow = document.querySelectorAll('.btn__icon_show');
-
-   btnShow.forEach((btn) => {
-      btn.addEventListener('click', async (e) => {
-         await showHistorial(e.target.id.split('_')[1]);
-         // Activa el modal
-         $('.container__modal').addClass('modal-active');
-      });
-   });
-}; */
 
 
 const pintarPaginacion = async () => {
@@ -454,17 +442,17 @@ const fillTable = async (data) => {
                            <ul class="btn_table_options">
                               <li>
                                  <a href="#" class="btn__icon_delete" id="btnDelete_${usuario.id}">
-                                    <svg class="noClickable" xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 -960 960 960" width="21" class="btn_icon"><path d="M292.309-140.001q-29.923 0-51.115-21.193-21.193-21.192-21.193-51.115V-720h-40v-59.999H360v-35.384h240v35.384h179.999V-720h-40v507.691q0 30.308-21 51.308t-51.308 21H292.309ZM680-720H280v507.691q0 5.385 3.462 8.847 3.462 3.462 8.847 3.462h375.382q4.616 0 8.463-3.846 3.846-3.847 3.846-8.463V-720ZM376.155-280h59.999v-360h-59.999v360Zm147.691 0h59.999v-360h-59.999v360ZM280-720v520-520Z"/></svg>
+                                    <svg class="Clickable" xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 -960 960 960" width="21" class="btn_icon"><path d="M292.309-140.001q-29.923 0-51.115-21.193-21.193-21.192-21.193-51.115V-720h-40v-59.999H360v-35.384h240v35.384h179.999V-720h-40v507.691q0 30.308-21 51.308t-51.308 21H292.309ZM680-720H280v507.691q0 5.385 3.462 8.847 3.462 3.462 8.847 3.462h375.382q4.616 0 8.463-3.846 3.846-3.847 3.846-8.463V-720ZM376.155-280h59.999v-360h-59.999v360Zm147.691 0h59.999v-360h-59.999v360ZM280-720v520-520Z"/></svg>
                                  </a>
                               </li>
                               <li>
                                  <a href="#" class="btn__icon_edit" id="btnEdit_${usuario.id}">
-                                    <svg class="noClickable" xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 -960 960 960" width="21" class="btn_icon"><path d="M200-200h50.461l409.463-409.463-50.461-50.461L200-250.461V-200Zm-59.999 59.999v-135.383l527.616-527.384q9.073-8.241 20.036-12.736 10.963-4.495 22.993-4.495 12.029 0 23.307 4.27 11.277 4.269 19.969 13.576l48.846 49.461q9.308 8.692 13.269 20.004 3.962 11.311 3.962 22.622 0 12.065-4.121 23.028-4.12 10.964-13.11 20.037l-527.384 527H140.001Zm620.384-570.153-50.231-50.231 50.231 50.231Zm-126.134 75.903-24.788-25.673 50.461 50.461-25.673-24.788Z"/></svg> 
+                                    <svg class="Clickable" xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 -960 960 960" width="21" class="btn_icon"><path d="M200-200h50.461l409.463-409.463-50.461-50.461L200-250.461V-200Zm-59.999 59.999v-135.383l527.616-527.384q9.073-8.241 20.036-12.736 10.963-4.495 22.993-4.495 12.029 0 23.307 4.27 11.277 4.269 19.969 13.576l48.846 49.461q9.308 8.692 13.269 20.004 3.962 11.311 3.962 22.622 0 12.065-4.121 23.028-4.12 10.964-13.11 20.037l-527.384 527H140.001Zm620.384-570.153-50.231-50.231 50.231 50.231Zm-126.134 75.903-24.788-25.673 50.461 50.461-25.673-24.788Z"/></svg> 
                                  </a>
                               </li>
                               <li>
-                                 <a href="#" class="btn__icon_show" id="btnShow_${usuario.id}">
-                                    <svg class="noClickable" xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 -960 960 960" width="21" class="btn_icon"><path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"/></svg>
+                                 <a href="#" class="btn__icon_show1" id="btnShow_${usuario.id}">
+                                    <svg class="Clickable" xmlns="http://www.w3.org/2000/svg" height="21" viewBox="0 -960 960 960" width="21" class="btn_icon"><path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"/></svg>
                                  </a>
                               </li>
                            </ul>
@@ -475,7 +463,7 @@ const fillTable = async (data) => {
    await pintarPaginacion();
    await addBtnDelete();
    await addBtnEdit();
-   //await addBtnShow();
+   await addBtnHistorial();
 };
 
 fillTable(await currentPageData());
@@ -542,18 +530,6 @@ const HistorialByUsuarioId = async (id) => {
    return historial;
 };
 
-//Agregar modal de historia de usuario
-const addBtnShow = async () => {
-   const btnShow = document.querySelectorAll('.btn__icon_show');
-
-   btnShow.forEach((btn) => {
-      btn.addEventListener('click', async (e) => {
-         await showHistorial(e.target.id.split('_')[1]);
-         // Activa el modal
-         $('.container__modal').addClass('modal-active');
-      });
-   });
-};
 
 
 const showHistorial = async (id) => {
@@ -615,7 +591,6 @@ const currentPageData1 = async () => {
    //Rellenamos el arreglo con los datos del servidor
    data.data.forEach((contra) => {
       userData1.push({
-         'usuario_id': contra.usuario_id,
          'usuario':contra.usuario,
          'correo': contra.correo
       });
@@ -643,14 +618,67 @@ const fillTable1 = async (data) => {
       document.querySelector('#demo1').innerHTML += `
                
                   <tr  class="tableOption"> 
-                        <td>${contra.id}</td>
                         <td>${contra.usuario}</td>
                         <td>${contra.correo}</td>
                      
                   </tr>`;
    });
    await pintarPaginacion1();
-   //await addBtnShow();
+   await addBtnHistorial();
 };
 
 fillTable1(await currentPageData1());
+
+
+//Agregar modal de historial de cambios
+const addBtnHistorial = async () => {
+   const btnShow1 = document.querySelectorAll('.btn__icon_show1');
+
+   btnShow1.forEach((btn) => {
+      btn.addEventListener('click', async (e) => {
+         await historialTable(e.target.id.split('_')[1]);
+         // Activa el modal
+         $('.container__modal1').addClass('modal-active');
+      });
+   });
+};
+
+//Mostrar tabla de historial
+
+const historialTable = async (id) => {
+   //Solicitamos los datos
+   currentData2 = await fetch('/solicitudes/get_all', {
+      method: 'POST', 
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+         //  start: `${index === undefined ? 0 : index * userPerPage}`,
+         //  length: userPerPage
+      })
+      });
+   console.log(data);
+   //Llenar Tabla modal
+   const tablaHistorial = document.querySelector('#table__modal');
+   tablaHistorial.innerHTML = '';
+   console.log(`Datos solicitados para el id ${id}`);
+   if (data.length === 0) {
+      tablaHistorial.innerHTML = `<tr>
+         <td>No hay polizas registradas</td>
+         <td></td>
+         <td></td>
+      </tr>`;
+   } else
+      data.forEach((poliza) => {
+         tablaHistorial.innerHTML += `
+                  <tr  class="tableOption">
+                        <td>${poliza.poliza}</td>
+                        <td>${poliza.ramo}</td>
+                        <td>${poliza.subramo}</td>
+                        <td>${poliza.primaNeta}</td>
+                        <td>${poliza.primaTotal}</td>
+                        <td>${poliza.fechaFin}</td>
+                        <td>${poliza.status}</td>
+                  </tr>`;
+      });
+};
