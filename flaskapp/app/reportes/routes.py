1a55130817183a721d00bc0fd2871ec70fbdabcd
Luis <you@example.com>
2025-01-09 11:11:01 -0600

    Recibos pagados

--- flaskapp/app/reportes/routes.py
+++ flaskapp/app/reportes/routes.py
@@ -940,7 +940,7 @@ def recibos_pagados():
         .filter(Recibo.fecha_inicio >= start_date,
                 Recibo.fecha_inicio <= end_date,
                 Recibo.status == "Liquidado") \
-        .order_by(Recibo.fecha_inicio)
+        .order_by(desc(Recibo.fecha_inicio))
 
     if polizas:
         paid_recipts_query = paid_recipts_query.filter(
